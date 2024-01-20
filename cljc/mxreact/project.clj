42989fff2ef4f0cc5ffeb18c495a74307a501cf4
lotuc <lotu.c@outlook.com>
2024-01-20 21:53:44 +0800

    chore(cljc/mxreact): setup clojars publishing
    - https://shadow-cljs.github.io/docs/UsersGuide.html#publish-lein

---
+++ cljc/mxreact/project.clj
@@ -0,0 +1,10 @@
+(defproject org.lotuc/mxreact "0.0.1-SNAPSHOT"
+  :description "mxreact"
+  :url "https://github.com/lotuc/matrix/tree/lotuc/main"
+  :license {:name "Eclipse Public License"
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies
+  []
+
+  :source-paths
+  ["src"])
